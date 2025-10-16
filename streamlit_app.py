a625d7cedb88e70afb8636ea63f2b153250d4973
mabhiramm <abhirammachammagari@gmail.com>
2025-10-16 15:11:42 +0530

    Update streamlit_app.py
    importing requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title("Smoothies App :tropical_drink:")
@@ -37,3 +38,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 
         st.success('Your Smoothie is ordered!')
+
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
